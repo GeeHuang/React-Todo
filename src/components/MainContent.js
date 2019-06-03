c343b946a26d7790c7defe32a0bc2f9c97c0d6ec
Dev Temp <devtemp@HC-iMac-IT.local>
2019-06-03 17:36:24 -0400

    Flip check for todoitems

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,10 +8,21 @@ export default class MainContent extends React.Component {
         this.state = {
             todos : todosData
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(id) {
-        console.log(id, 'Change');
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id){
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+            return {
+                todos: updatedTodos
+            }
+        });
     }
 
     render(){
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,5 @@
 import React from 'react'
 
-// export default class TodoItem extends React.Component {
-//     render() {
-//         return (
-//             <div className='todo-item'>
-//                 <input type='checkbox' checked={this.props.item.completed} onChange={() => this.props.handleChange(this.props.item.id)}/>
-//                 <p>{this.props.item.text}</p>
-//             </div>
-//         );
-//     };
-// };
-
 function TodoItem(props) {
 
     return (
