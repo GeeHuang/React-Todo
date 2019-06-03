b701f11c5258c6807e9004e00ee004d841224e84
Dev Temp <devtemp@HC-iMac-IT.local>
2019-06-03 17:27:56 -0400

    TodoItem Component Updates

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-export default class TodoItem extends React.Component {
-    render() {
-        return (
-            <div className='todo-item'>
-                <input type='checkbox' checked={this.props.item.completed} onChange={() => this.props.handleChange(this.props.item.id)}/>
-                <p>{this.props.item.text}</p>
-            </div>
-        );
-    };
-};
+// export default class TodoItem extends React.Component {
+//     render() {
+//         return (
+//             <div className='todo-item'>
+//                 <input type='checkbox' checked={this.props.item.completed} onChange={() => this.props.handleChange(this.props.item.id)}/>
+//                 <p>{this.props.item.text}</p>
+//             </div>
+//         );
+//     };
+// };
+
+function TodoItem(props) {
+
+    return (
+        <div className='todo-item'>
+            <input type='checkbox' checked={props.item.completed} onChange={() => props.handleChange(props.item.id)}/>
+            <p>{props.item.text}</p>
+        </div>
+    );
+}
+
+export default TodoItem;
